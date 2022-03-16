73af65b6b8bb5adf38d44bf9a9f2312ec919b530
Amanda Lange <litagemini@gmail.com>
2022-03-15 20:59:56 -0400

    Length algo

--- linkedLists3.js
+++ linkedLists3.js
@@ -22,6 +22,46 @@ class LinkedList{
         return this.head.value;
     }
 }
+    length(){
+        if(this.head == null){
+            return 0;
+        }
+        let runner = this.head;
+        let count = 0;
+        while(runner != null){
+            count++;
+            runner = runner.next;
+        }
+        return count;
+    }
+}
 
 
+
+
+
+sll1 = new LinkedList();
+
+node1 = new Node(8);
+node2 = new Node(5);
+
+sll1.head = node1;
+node1.next = node2;
+
+//interating - variations
+
+for(let i = 1; i <=5; i++){
+    //some other stuff
+}
+
+let i = 1;
+while(i <= 5){
+    //do whatever stuff
+    i++;
+}
+
+let runner = this.head;
+while (runner != null){ //make sure it's not null or the list is over
+    //do whatever stuff
+    runner = runner.next; //moving to the next node in the line
 }
